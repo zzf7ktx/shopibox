63013ace164eb7a848608e1bc678f3b7c006ac1d
zzf7ktx <zzf7ktx@gmail.com>
2024-01-07 19:12:56 +0700

    [bug] - fix error in the nav menu

--- app/main/layout.tsx
+++ app/main/layout.tsx
@@ -1,31 +1,23 @@
 "use client";
-// import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Button, Layout, Menu, theme } from "antd";
 import {
-  UserOutlined,
   VideoCameraOutlined,
   UploadOutlined,
   MenuUnfoldOutlined,
   MenuFoldOutlined,
-  ShoppingCartOutlined,
   ShoppingOutlined,
 } from "@ant-design/icons";
 import { Suspense, useState } from "react";
 import "./main.css";
-import Link from "next/link";
-import MenuItem from "antd/es/menu/MenuItem";
 import { usePathname, useRouter } from "next/navigation";
 import Loading from "./loading";
+import { ItemType, MenuItemType } from "antd/es/menu/hooks/useItems";
 
 const { Header, Sider, Content } = Layout;
 
 const inter = Inter({ subsets: ["latin"] });
 
-// export const metadata: Metadata = {
-//   title: "Image management",
-//   description: "Image management",
-// };
 
 export default function MainLayout({
   children,
@@ -37,22 +29,38 @@ export default function MainLayout({
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
   const pathName = usePathname();
+  const router = useRouter();
+
+  const items: ItemType<MenuItemType>[] = [
+    {
+      key: "/main/products",
+      icon: <ShoppingOutlined />,
+      label: "Product",
+      onClick: () => router.replace("/main/products"),
+    },
+    {
+      key: "/main/images",
+      icon: <VideoCameraOutlined />,
+      label: "Images",
+      onClick: () => router.replace("/main/images"),
+    },
+    {
+      key: "soon",
+      icon: <UploadOutlined />,
+      label: "Coming Soon",
+    },
+  ];
 
   return (
     <Layout className="h-screen" hasSider>
       <Sider trigger={null} collapsible collapsed={collapsed}>
-        <div className="logo">Shopify Tools</div>
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={[pathName]}>
-          <MenuItem key="/main/products" icon={<ShoppingOutlined />}>
-            <Link href="products">Products</Link>
-          </MenuItem>
-          <MenuItem key="/main/images" icon={<VideoCameraOutlined />}>
-            <Link href="images">Images</Link>
-          </MenuItem>
-          <MenuItem key="3" icon={<UploadOutlined />}>
-            Comming soon
-          </MenuItem>
-        </Menu>
+        <div className="logo">Shopibox</div>
+        <Menu
+          items={items}
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={[pathName]}
+        ></Menu>
       </Sider>
       <Layout>
         <Header style={{ padding: 0, background: colorBgContainer }}>
