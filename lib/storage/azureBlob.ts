cffeff55b3a6d4811a36edde794f1f70bfb639b4
zzf7ktx <zzf7ktx@gmail.com>
2024-12-26 23:37:48 +0700

    [feature] - add azure as primary storage provider

--- lib/storage/azureBlob.ts
+++ lib/storage/azureBlob.ts
@@ -4,24 +4,25 @@ import { v1 as uuidv1 } from "uuid";
 import urlUtil from "url";
 import path from "path";
 
-// const connectionString = process.env.AZURE_BLOB_CONNECTION_STRING ?? "";
-// const blobServiceClient =
-//   BlobServiceClient.fromConnectionString(connectionString);
-// const containerClient = blobServiceClient.getContainerClient("shopibox");
+const connectionString = process.env.AZURE_BLOB_CONNECTION_STRING ?? "";
+const blobServiceClient = connectionString
+  ? BlobServiceClient.fromConnectionString(connectionString)
+  : new BlobServiceClient("http://localhost:3000");
+const containerClient = blobServiceClient.getContainerClient("shopibox");
 
 const upload = async (
   url: string,
   options: UploadOptions
 ): Promise<UploadResult> => {
-  // const parsedUrl = new urlUtil.URL(url);
-  // const extension = path.extname(parsedUrl.pathname);
-  // const blobName = options.overwrite ? options.publicId : uuidv1() + extension;
-  // const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  // const result = await blockBlobClient.syncUploadFromURL(url);
+  const parsedUrl = new urlUtil.URL(url);
+  const extension = path.extname(parsedUrl.pathname);
+  const blobName = options.overwrite ? options.publicId : uuidv1() + extension;
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const result = await blockBlobClient.syncUploadFromURL(url);
 
   return {
-    publicId: "blobName",
-    secureUrl: "blockBlobClient.url",
+    publicId: blobName,
+    secureUrl: blockBlobClient.url,
   };
 };
 
@@ -29,34 +30,34 @@ const uploadFile = async (
   file: File,
   options: UploadOptions
 ): Promise<UploadResult> => {
-  // const parsedUrl = new urlUtil.URL(file.name);
-  // const extension = path.extname(parsedUrl.pathname);
-  // const blobName = options.overwrite ? options.publicId : uuidv1() + extension;
-  // const blockBlobClient = containerClient.getBlockBlobClient(blobName);
-  // const result = await blockBlobClient.uploadData(file);
+  const parsedUrl = new urlUtil.URL(file.name);
+  const extension = path.extname(parsedUrl.pathname);
+  const blobName = options.overwrite ? options.publicId : uuidv1() + extension;
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const result = await blockBlobClient.uploadData(file);
 
   return {
-    publicId: "blobName",
-    secureUrl: "blockBlobClient.url",
+    publicId: blobName,
+    secureUrl: blockBlobClient.url,
   };
 };
 
 const remove = async (id: string) => {
-  // const blockBlobClient = containerClient.getBlockBlobClient(id);
-  // const result = await blockBlobClient.delete();
+  const blockBlobClient = containerClient.getBlockBlobClient(id);
+  const result = await blockBlobClient.delete();
 };
 
 const get = async (id: string): Promise<GetResult> => {
-  // const blockBlobClient = containerClient.getBlockBlobClient(id);
-  // const properties = await blockBlobClient.getProperties();
+  const blockBlobClient = containerClient.getBlockBlobClient(id);
+  const properties = await blockBlobClient.getProperties();
   return {
     assetId: id,
-    bytes: 0,
-    fileType: "",
-    originalFilename: "blockBlobClient.name",
+    bytes: properties.contentLength ?? 0,
+    fileType: properties.contentType ?? "",
+    originalFilename: blockBlobClient.name,
     publicId: id,
-    secureUrl: "blockBlobClient.url",
-    url: "blockBlobClient.url",
+    secureUrl: blockBlobClient.url,
+    url: blockBlobClient.url,
   };
 };
 
