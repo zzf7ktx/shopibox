84e2007c4556c43d9d168491595fa78b836cf015
zzf7ktx <41227962+zzf7ktx@users.noreply.github.com>
2025-09-01 16:18:14 +0000

    [feature] - update shop page loading

--- app/main/shops/[id]/products/loading.tsx
+++ app/main/shops/[id]/products/loading.tsx
@@ -1,11 +1,11 @@
-import JobTable from "@/components/JobTable";
+import ProductTable from "@/components/ProductTable";
 
 export default function Loading() {
   return <div className='flex-1 space-y-4 py-6'>
     <div className='flex items-center justify-between space-y-2'>
-      <h2 className='text-3xl font-bold tracking-tight'>Jobs</h2>
+      <h2 className='text-3xl font-bold tracking-tight'>Products</h2>
       <div className='flex items-center space-x-2'></div>
     </div>
-    <JobTable loading />;
+    <ProductTable loading />;
   </div>
 }
